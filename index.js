02242003409bddcebe4729bc8ed7d4bee52d877d
mishkadong <110600967+mishkadong@users.noreply.github.com>
2022-09-10 13:45:08 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,60 +1,91 @@
 "use strict";
-let area = document.querySelector(".area");
-let box = document.querySelector(".box");
-let circle = document.createElement("div");
-let scoreBoard = document.createElement("span");
-let score = 0;
-let counter = 600;
-let interval;
-area.prepend(scoreBoard);
-scoreBoard.style.cssText = `
-display: none;
-position: absolute;
-`;
-scoreBoard.appendChild(document.createTextNode(`score: ${score}`));
-box.addEventListener("click", theGame);
+const area = document.querySelector("body"),
+  box = document.querySelector(".box"),
+  boxEnd = document.createElement("button"),
+  circle = document.createElement("div"),
+  scoreBoard = document.createElement("span");
+let score = 0,
+  counter = 600,
+  interval,
+  circles = document.getElementsByName("circle"); // need live collection to use "if"
+
+box.innerHTML = "Гайда шмаляти русню";
+
+area.append(boxEnd, scoreBoard);
+
+boxEnd.classList.add("box");
+boxEnd.innerHTML = "Почати давити русню спочатку";
+boxEnd.hidden = true;
+
+scoreBoard.hidden = true;
+scoreBoard.append(`пiдстрелено свинособак: ${score}`);
+
 function getRandomNumber(min, max) {
   let number = Math.floor(Math.random() * (max - min + 1));
   return number;
-}
+} //get random number
+
 function theGame() {
-  box.style.display = "none";
-  scoreBoard.style.display = "unset";
+  box.hidden = true;
+  scoreBoard.hidden = false;
   interval = setInterval(createCircle, 600);
-}
+} //additional function to change intervals
+
 function createCircle() {
   let newCircle = circle.cloneNode(true);
-  area.prepend(newCircle);
+  area.append(newCircle);
   newCircle.classList.add("circle");
   newCircle.setAttribute("name", "circle");
+  newCircle.innerHTML = `<img src="./images/pig.webp" class="img" alt="russian pig" />`;
   newCircle.style.cssText = `
-  left: ${getRandomNumber(50, document.documentElement.clientWidth)}px;
-  top: ${getRandomNumber(50, document.documentElement.clientHeight)}px;
+  left: ${getRandomNumber(70, document.documentElement.clientWidth)}px;
+  top: ${getRandomNumber(70, document.documentElement.clientHeight)}px;
   background-color: rgb(${getRandomNumber(0, 255)}, ${getRandomNumber(
     0,
     255
   )}, ${getRandomNumber(0, 255)});
   `;
-  let circles = document.getElementsByName("circle");
-  if (circles.length === 100) {
-    alert(`GAME OVER\nYour score is ${score}`);
-    document.querySelectorAll(".circle").forEach((e) => {
-      e.remove();
-    });
-    location.reload();
+
+  if (circles.length === 30) {
+    clearInterval(interval);
+
+    area.removeEventListener("click", pop);
+    alert("Їх тут як лайна! Давай останніх гранатою підірвемо!");
+
+    area.addEventListener("click", popAnimation);
   }
-}
-area.addEventListener("click", (e) => {
-  if (e.target.classList == "circle") {
-    e.target.remove();
-    score += 1;
+} //create circle
+
+function pop(e) {
+  if (e.target.closest(".circle")) {
+    e.target.closest(".circle").classList.add("circle__pop");
+    let popCircle = () => {
+      e.target.closest(".circle").remove();
+    };
+    setTimeout(popCircle, 200);
     counter -= 3;
-    scoreBoard.textContent = `score: ${score}`;
-    while (counter > 250) {
+    scoreBoard.textContent = `пiдстрелено свинособак: ${++score}`;
+    while (counter > 225) {
       clearInterval(interval);
       interval = setInterval(createCircle, counter);
-      console.log(counter);
       break;
     }
   }
-});
+} //pop circle
+
+function popAllCircles() {
+  score += circles.length;
+  circles.forEach((e) => e.remove());
+  boxEnd.hidden = false;
+  scoreBoard.textContent = `ПЕРЕМОГА! Вбито та пiдiрвано свинособак: ${score}!`;
+} //pop all circle
+
+function popAnimation() {
+  circles = document.querySelectorAll(".circle");
+  circles.forEach((e) => e.classList.add("circle__pop-all"));
+  setTimeout(popAllCircles, 150);
+}
+
+box.addEventListener("click", theGame);
+boxEnd.addEventListener("click", () => location.reload());
+area.addEventListener("click", pop);
