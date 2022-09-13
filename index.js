012c3c9da42742a8c339eb810131604210b6b021
mishkadong <110600967+mishkadong@users.noreply.github.com>
2022-09-13 21:34:49 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const area = document.querySelector("body"),
   box = document.querySelector(".box"),
   boxEnd = document.createElement("button"),
   circle = document.createElement("div"),
-  scoreBoard = document.createElement("span");
+  scoreBoard = document.createElement("span"),
+  mode_1 = document.createElement("button"),
+  mode_2 = document.createElement("button");
 let score = 0,
   counter = 600,
+  counter_2 = 0,
   interval,
   circles = document.getElementsByName("circle"); // need live collection to use "if"
 
-box.innerHTML = "Гайда шмаляти русню";
+box.innerHTML = "Гайда шмаляти русню...";
 
-area.append(boxEnd, scoreBoard);
+area.append(boxEnd, scoreBoard, mode_1, mode_2);
 
 boxEnd.classList.add("box");
 boxEnd.innerHTML = "Почати давити русню спочатку";
@@ -20,72 +23,114 @@ boxEnd.hidden = true;
 scoreBoard.hidden = true;
 scoreBoard.append(`пiдстрелено свинособак: ${score}`);
 
+mode_1.innerHTML = `...як Джиммі Рінго`;
+mode_1.classList.add("box-2");
+mode_1.hidden = true;
+
+mode_2.innerHTML = `...як Рембо`;
+mode_2.classList.add("box-3");
+mode_2.hidden = true;
+
 function getRandomNumber(min, max) {
-  let number = Math.floor(Math.random() * (max - min + 1));
+  let number = Math.floor(Math.random() * (max - min + 1)) + min;
   return number;
 } //get random number
 
-function theGame() {
-  box.hidden = true;
+function theGame_1() {
+  createCircle();
+
+  area.addEventListener("click", pop_1);
+
+  if (circles.length == 25) {
+    clearInterval(interval);
+
+    area.removeEventListener("click", pop_1);
+    alert("Їх тут як лайна! Давай останніх гранатою підірвемо!");
+
+    area.addEventListener("click", popAll);
+  }
+} //start mode_1 game
+
+function theGame_2() {
+  mode_1.hidden = true;
+  mode_2.hidden = true;
   scoreBoard.hidden = false;
-  interval = setInterval(createCircle, 600);
-} //additional function to change intervals
+  // for (let i = 0; i < 100; i++) {
+  while (circles.length < 100) createCircle();
+  // }
+  area.addEventListener("click", pop_2);
+} //start mode_2 game
 
 function createCircle() {
-  let newCircle = circle.cloneNode(true);
+  const newCircle = circle.cloneNode(true);
   area.append(newCircle);
   newCircle.classList.add("circle");
   newCircle.setAttribute("name", "circle");
   newCircle.innerHTML = `<img src="./images/pig.webp" class="img" alt="russian pig" />`;
   newCircle.style.cssText = `
-  left: ${getRandomNumber(70, document.documentElement.clientWidth)}px;
-  top: ${getRandomNumber(70, document.documentElement.clientHeight)}px;
+  left: ${getRandomNumber(0, document.documentElement.clientWidth - 110)}px;
+  top: ${getRandomNumber(0, document.documentElement.clientHeight - 110)}px;
   background-color: rgb(${getRandomNumber(0, 255)}, ${getRandomNumber(
     0,
     255
   )}, ${getRandomNumber(0, 255)});
   `;
-
-  if (circles.length === 30) {
-    clearInterval(interval);
-
-    area.removeEventListener("click", pop);
-    alert("Їх тут як лайна! Давай останніх гранатою підірвемо!");
-
-    area.addEventListener("click", popAnimation);
-  }
 } //create circle
 
-function pop(e) {
+function pop_1(e) {
   if (e.target.closest(".circle")) {
     e.target.closest(".circle").classList.add("circle__pop");
-    let popCircle = () => {
+    setTimeout(() => {
       e.target.closest(".circle").remove();
-    };
-    setTimeout(popCircle, 200);
-    counter -= 3;
+    }, 200);
+    counter -= 4;
     scoreBoard.textContent = `пiдстрелено свинособак: ${++score}`;
-    while (counter > 225) {
+    while (counter >= 200) {
       clearInterval(interval);
-      interval = setInterval(createCircle, counter);
+      interval = setInterval(theGame_1, counter);
       break;
     }
   }
-} //pop circle
+} //pop circle in mode_1
 
-function popAllCircles() {
+function pop_2(e) {
+  if (e.target.closest(".circle")) {
+    e.target.closest(".circle").classList.add("circle__pop-all");
+    setTimeout(() => {
+      e.target.closest(".circle").remove();
+    }, 200);
+    scoreBoard.textContent = `пiдстрелено свинособак: ${++score}`;
+    counter_2++;
+  }
+  if (counter_2 === 100) {
+    boxEnd.hidden = false;
+    scoreBoard.textContent = `ПЕРЕМОГА! Вбито та пiдiрвано свинособак: ${score}!`;
+  }
+} //pop circle in mode_2
+
+function popAll() {
+  circles = document.querySelectorAll(".circle");
   score += circles.length;
-  circles.forEach((e) => e.remove());
+  circles.forEach((e) => e.classList.add("circle__pop-all"));
+  setTimeout(() => circles.forEach((e) => e.remove()), 150);
+  area.addEventListener("click", () => console.log(123));
   boxEnd.hidden = false;
   scoreBoard.textContent = `ПЕРЕМОГА! Вбито та пiдiрвано свинособак: ${score}!`;
-} //pop all circle
+} // pop all circles with animation
 
-function popAnimation() {
-  circles = document.querySelectorAll(".circle");
-  circles.forEach((e) => e.classList.add("circle__pop-all"));
-  setTimeout(popAllCircles, 150);
-}
+box.addEventListener("click", () => {
+  box.hidden = true;
+  mode_1.hidden = false;
+  mode_2.hidden = false;
+});
 
-box.addEventListener("click", theGame);
+mode_1.addEventListener("click", () => {
+  mode_1.hidden = true;
+  mode_2.hidden = true;
+  scoreBoard.hidden = false;
+  interval = setInterval(theGame_1, counter);
+});
+
+mode_2.addEventListener("click", theGame_2);
+
 boxEnd.addEventListener("click", () => location.reload());
-area.addEventListener("click", pop);
